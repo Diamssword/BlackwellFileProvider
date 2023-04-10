de1ea5db63d023d5b79bfc8dcb09e18d9a863977
Stonksword <72971712+Stonksword@users.noreply.github.com>
2023-04-10 13:55:38 +0200

    Systeme de serveur itnegré

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import 'dotenv/config'
+import * as path from 'path'
+import * as fs from 'fs'
+import {initRootCommand,generateDistroCommand} from './helios.js'
+const port= process.env.port||3000
+let app=express();
+process.env.ROOT=path.resolve("packs")
+if(!fs.existsSync("packs"))
+{
+    console.log("creating base pack folder")
+    fs.mkdirSync("packs")
+    initRootCommand.handler({} as any)
+}
+setInterval(()=>{
+    generateDistroCommand.handler({} as any)
+},60*1000)//reload toute les minutes
+generateDistroCommand.handler({} as any)
+app.use("/files/",express.static("packs"))
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+  })
