ef005d413102fc91e236c34153ca5e51a1db16bb
Stonksword <72971712+Stonksword@users.noreply.github.com>
2023-04-10 16:21:05 +0200

    Update JavaUtil.ts

--- src/util/java/JavaUtil.ts
+++ src/util/java/JavaUtil.ts
@@ -1,6 +1,7 @@
 export class JavaUtil {
 
     public static getJavaExecutable(): string {
+        console.log(process.env.JAVA_EXECUTABLE)
         return process.env.JAVA_EXECUTABLE as string
     }
 
